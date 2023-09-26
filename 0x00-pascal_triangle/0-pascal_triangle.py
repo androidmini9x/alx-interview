7270de76c9ef31544c7e714393772c8e7aebfd0c
Androidmini9x <androidmini92@gmail.com>
2023-09-27 01:32:05 +0300

    Solve: 0x00

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+The function that returns a list of lists of integers
+representing the Pascals triangle of n:
+"""
+
+
+def pascal_triangle(n):
+    tri_list = []
+
+    # empty list if n <= 0
+    if n <= 0:
+        return tri_list
+    
+    for x in range(n):
+        tri_list.append([1] * (x+1))
+        for y in range(1, x):
+            tri_list[x][y] = tri_list[x-1][y] + tri_list[x-1][y-1]
+
+    return tri_list
