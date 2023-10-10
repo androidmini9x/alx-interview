5e1c90dd542dcd854f9d9152e9ed725546bd4df0
atck3r <atck3r@localdomain>
2023-10-10 21:38:58 +0300

    Fixed

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,22 +7,22 @@ to result in exactly n
 
 def minOperations(n):
     '''
-    minOperations
-    Gets fewest # of operations needed to result in exactly n H characters
+    Calculates the fewest number of operations needed
+    to result in exactly n
     '''
-    # all outputs should be at least 2 char: (min, Copy All => Paste)
-    if (n < 2):
+    if n < 2:
         return 0
-    ops, root = 0, 2
-    while root <= n:
-        # if n evenly divides by root
-        if n % root == 0:
-            # total even-divisions by root = total operations
-            ops += root
-            # set n to the remainder
-            n = n / root
-            # reduce root to find remaining smaller vals that evenly-divide n
-            root -= 1
-        # increment root until it evenly-divides n
-        root += 1
-    return ops
+    steps = 0
+    buffer = 0
+    text = 1
+    while (text < n):
+        if (n % text == 0):
+            # Copy All & Paste
+            buffer = text
+            text += buffer
+            steps += 2
+        else:
+            # Only Paste
+            text += buffer
+            steps += 1
+    return steps
