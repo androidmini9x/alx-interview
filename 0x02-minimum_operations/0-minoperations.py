bcc5bebdc57b2360d3e4f1cd7a44add29d7e1b7c
atck3r <atck3r@localdomain>
2023-10-10 21:17:58 +0300

    Solve: 0x02

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+'''
+0. Minimum Operations
+'''
+
+
+def minOperations(n):
+    '''
+    Calculates the fewest number of operations needed
+    to result in exactly n
+    '''
+    steps = 0
+    buffer = ""
+    text = "H"
+    while (len(text) < n):
+        if (n % len(text) == 0):
+            # Copy All & Paste
+            buffer = text
+            text += buffer
+            steps += 2
+        else:
+            # Only Paste
+            text += buffer
+            steps += 1
+    return steps
