f1091127ef6e97f07e29d9914d84d2356659aab6
Androidmini9x <androidmini92@gmail.com>
2023-10-19 05:22:53 +0300

    Solve: 0x03

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+'''Script reads stdin line by line and computes metrics
+<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
+'''
+import re
+import sys
+
+
+def parse_line(line):
+    '''Split line to tokens'''
+    pattern = r'(?P<ip>^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - ' \
+        r'\[(?P<date>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6})] ' \
+        r'"(?P<req>.*)" (?P<code>\d{3}) (?P<file_size>\d{1,4})$'
+    tokens = re.match(pattern, line)
+    return tokens
+
+
+def print_static(status, size):
+    print(f'File size: {size}')
+    for k in status:
+        if status[k] != 0:
+            print('{}: {}'.format(k, status[k]))
+
+
+def stats():
+    '''Parse stdin line by line'''
+    counter = 0
+    size = 0
+    status = {'200': 0, '301': 0, '400': 0, '401': 0,
+              '403': 0, '404': 0, '405': 0, '500': 0}
+
+    try:
+        for line in sys.stdin:
+            counter += 1
+            parse = parse_line(line)
+            if not parse:
+                continue
+            tokens = parse.groupdict()
+            if tokens['code'] in status:
+                status[tokens['code']] += 1
+            size += int(tokens['file_size'])
+            if counter % 10 == 0:
+                print_static(status, size)
+
+    except (KeyboardInterrupt, SystemExit):
+        print_static(status, size)
+
+
+if __name__ == '__main__':
+    stats()
