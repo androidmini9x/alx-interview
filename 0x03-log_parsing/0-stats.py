c8d9d15fb37f163e4599103026a5652a4b1cd8d9
atck3r <atck3r@localdomain>
2023-10-19 19:23:13 +0300

    Fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,51 +1,29 @@
 #!/usr/bin/python3
-'''Script reads stdin line by line and computes metrics
-'''
-import re
+"""Computes metrics"""
 import sys
 
-
-def parse_line(line):
-    '''Split line to tokens'''
-    tokens = line.split(' ')
-    return tokens
-
-
-def print_static(status, size):
-    '''Print statistic logs'''
-    print(f'File size: {size}')
-    for k in sorted(status.keys()):
-        if status[k] != 0:
-            print('{}: {}'.format(k, status[k]), flush=True)
-
-
-def stats():
-    '''Parse stdin line by line'''
-    counter = 0
-    size = 0
-    status = {'200': 0, '301': 0, '400': 0, '401': 0,
-              '403': 0, '404': 0, '405': 0, '500': 0}
-
+if __name__ == "__main__":
+    counter = 1
+    sum = 0
+    metrics = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
     try:
         for line in sys.stdin:
-            counter += 1
-            tokens = parse_line(line)
-            if not tokens[-1].strip().isdigit():
-                continue
-            file_size = int(tokens[-1])
-            code = tokens[-2]
-            if code in status:
-                status[code] += 1
-                size += file_size
+            # line = line.rstrip()
+            line = line.split(" ")
+            sum += int(line[-1])
+            code = int(line[-2])
+            if code in metrics:
+                metrics[code] += 1
             if counter % 10 == 0:
-                print_static(status, size)
-        else:
-            print_static(status, size)
-
-    except (KeyboardInterrupt, SystemExit):
-        print_static(status, size)
-        raise
-
-
-if __name__ == '__main__':
-    stats()
+                print("File size: {}".format(sum))
+                for key, value in sorted(metrics.items()):
+                    if (value):
+                        print("{}: {}".format(key, value))
+            counter += 1
+    except Exception:
+        pass
+    finally:
+        print("File size: {}".format(sum))
+        for key, value in sorted(metrics.items()):
+            if (value):
+                print("{}: {}".format(key, value))
