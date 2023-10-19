8a1b9974711a9dd9226ed4c1b5a785ba44b14fd8
atck3r <atck3r@localdomain>
2023-10-19 19:26:07 +0300

    Fixed

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,29 +1,45 @@
 #!/usr/bin/python3
-"""Computes metrics"""
 import sys
 
-if __name__ == "__main__":
-    counter = 1
-    sum = 0
-    metrics = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-    try:
-        for line in sys.stdin:
-            # line = line.rstrip()
-            line = line.split(" ")
-            sum += int(line[-1])
-            code = int(line[-2])
-            if code in metrics:
-                metrics[code] += 1
-            if counter % 10 == 0:
-                print("File size: {}".format(sum))
-                for key, value in sorted(metrics.items()):
-                    if (value):
-                        print("{}: {}".format(key, value))
-            counter += 1
-    except Exception:
-        pass
-    finally:
-        print("File size: {}".format(sum))
-        for key, value in sorted(metrics.items()):
-            if (value):
-                print("{}: {}".format(key, value))
+# Initialize variables
+codes = {}
+status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+count = 0
+size = 0
+
+
+def print_statistics(size, codes):
+    print("File size: {}".format(size))
+    for key in sorted(codes):
+        print("{}: {}".format(key, codes[key]))
+
+
+try:
+    for ln in sys.stdin:
+        if count == 10:
+            print_statistics(size, codes)
+            count = 1
+        else:
+            count += 1
+
+        ln = ln.split()
+
+        try:
+            size = size + int(ln[-1])
+        except (IndexError, ValueError):
+            pass
+
+        try:
+            if ln[-2] in status_codes:
+                if ln[-2] not in codes:
+                    codes[ln[-2]] = 1
+                else:
+                    codes[ln[-2]] += 1
+        except IndexError:
+            pass
+
+    print_statistics(size, codes)
+
+except KeyboardInterrupt:
+    print_statistics(size, codes)
+    raise
