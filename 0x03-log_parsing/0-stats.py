550f7bee90c240c13c0b8aaf68aa87999646f6dd
Androidmini9x <androidmini92@gmail.com>
2023-10-19 06:07:04 +0300

    Fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,52 +1,41 @@
 #!/usr/bin/python3
-'''Script reads stdin line by line and computes metrics
-'''
-import re
+"""module docs"""
 import sys
 
 
-def parse_line(line):
-    '''Split line to tokens'''
-    pattern = r'(?P<ip>^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - ' \
-        r'\[(?P<date>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6})] ' \
-        r'"(?P<req>.*)" (?P<code>\S+) (?P<file_size>\d+)$'
-    tokens = re.match(pattern, line)
-    return tokens
+def handleTen(statCount, fileSize):
+    print("File size: {}".format(fileSize))
+    for key in sorted(statCount.keys()):
+        if statCount[key] == 0:
+            continue
+        print("{}: {}".format(key, statCount[key]))
 
 
-def print_static(status, size):
-    '''Print statistic logs'''
-    print(f'File size: {size}')
-    for k in sorted(status.keys()):
-        if status[k] != 0:
-            print('{}: {}'.format(k, status[k]), flush=True)
-
-
-def stats():
-    '''Parse stdin line by line'''
-    counter = 0
-    size = 0
-    status = {'200': 0, '301': 0, '400': 0, '401': 0,
-              '403': 0, '404': 0, '405': 0, '500': 0}
+def log_parsing():
+    """function docs"""
+    c = size = 0
+    counter = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+               "404": 0, "405": 0, "500": 0}
 
     try:
         for line in sys.stdin:
-            counter += 1
-            parse = parse_line(line)
-            if not parse:
-                continue
-            tokens = parse.groupdict()
-            if tokens['code'] in status:
-                status[tokens['code']] += 1
-                size += int(tokens['file_size'])
-            if counter % 10 == 0:
-                print_static(status, size)
+            c += 1
+            split = line.split(" ")
+            try:
+                status = split[-2]
+                size += int(split[-1])
+                if status in counter:
+                    counter[status] += 1
+            except Exception:
+                pass
+            if c % 10 == 0:
+                handleTen(counter, size)
         else:
-            print_static(status, size)
-
+            handleTen(counter, size)
     except (KeyboardInterrupt, SystemExit):
-        print_static(status, size)
+        handleTen(counter, size)
+        raise
 
 
-if __name__ == '__main__':
-    stats()
+if __name__ == "__main__":
+    log_parsing()
