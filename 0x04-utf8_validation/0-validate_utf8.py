fa7c608c10da157716627f8c13ecd488d0cc963e
atck3r <atck3r@localdomain>
2023-10-26 20:45:35 +0300

    Solve: 0x04

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''0. UTF-8 Validation
+'''
+
+
+def validUTF8(data):
+    '''
+    determines if a given data set represents a valid UTF-8 encoding
+    '''
+    # tracking the most signicate bits
+    n = 0
+
+    for bt in data:
+        if n == 0:
+            mask = 128
+            while (mask & bt):
+                n += 1
+                mask >>= 1
+
+            if n == 1 or n > 4:
+                return False
+
+            if n == 0:
+                continue
+        else:
+            if not (bt & 128 and not (bt & 64)):
+                return False
+
+    return n == 0
