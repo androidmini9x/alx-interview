82593c16027c7362cc8143408fd1b22f027b2b04
atck3r <atck3r@localdomain>
2023-11-07 18:56:46 +0300

    Fixed

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,28 +1,28 @@
 #!/usr/bin/node
-const request = require('request');
-const API_URL = 'https://swapi-api.alx-tools.com/api';
+const request = require('request')
+const API_URL = 'https://swapi-api.alx-tools.com/api'
 
 if (process.argv.length > 2) {
-  const movieID = process.argv[2];
+  const movieID = process.argv[2]
   request(`${API_URL}/films/${movieID}`, (error, resp, body) => {
-    if (error || resp.statusCode != 200) {
-      console.log(error);
+    if (error || resp.statusCode !== 200) {
+      console.log(error)
     } else {
-      const characters = JSON.parse(body).characters;
+      const characters = JSON.parse(body).characters
       const fetchCharacters = characters.map(c => {
-        return new Promise((resolve, rej) => {
-          request(c, (err, res, charBody) => {
+        return new Promise((resolve, reject) => {
+          request(c, (err, _, charBody) => {
             if (err) {
-              rej(err);
+              reject(err)
             }
-            resolve(JSON.parse(charBody).name);
-          });
-        });
-      });
+            resolve(JSON.parse(charBody).name)
+          })
+        })
+      })
 
       Promise.all(fetchCharacters)
         .then(name => console.log(name.join('\n')))
         .catch(err => console.log(err))
     }
-  });
+  })
 }
