42f5a75257854aedfc50f2f65061fed825f56d1f
Androidmini9x <androidmini92@gmail.com>
2023-12-04 22:53:46 +0300

    Solve: 0x0A

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+''' 0. Prime Game
+'''
+
+
+def isWinner(x, nums):
+    ''''''
+    player = {'Maria': 0, 'Ben': 0}
+    for i in range(x):
+        idx = -1
+        lst = [k for k in range(1, nums[i]+1)]
+        for j in lst:
+            if isPrime(j):
+                lst.remove(j)
+                idx += 1
+        if idx % 2 == 1:
+            player['Ben'] += 1
+        else:
+            player['Maria'] += 1
+    if player['Ben'] > player['Maria']:
+        return 'Ben'
+    else:
+        return 'Maria'
+
+
+def isPrime(num):
+    '''Return True if num is prime'''
+    if num <= 1:
+        return False
+    for i in range(num//2):
+        if num % 2 == 0:
+            return False
+    return True
