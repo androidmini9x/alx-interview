9778ae8d176f6c4e506b6da54a3f42f8b44366f5
atck3r <atck3r@localdomain>
2023-12-05 15:10:40 +0300

    Fixed: Prime func

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -32,7 +32,7 @@ def isPrime(num):
     '''Return True if num is prime'''
     if num <= 1:
         return False
-    for i in range(num//2):
-        if num % 2 == 0:
+    for i in range(2, num//2):
+        if num % i == 0:
             return False
     return True
