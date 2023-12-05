37429488be5438fa8439955bf8f5e7dfd60edf2c
atck3r <atck3r@localdomain>
2023-12-05 15:41:41 +0300

    Fixed

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -7,16 +7,17 @@ def isWinner(x, nums):
     '''Return the winner'''
     player = {'Maria': 0, 'Ben': 0}
     for i in range(x):
-        idx = -1
+        idx = 0
         lst = [k for k in range(1, nums[i]+1)]
         for j in lst:
             if isPrime(j):
                 idx += 1
-        if idx % 2 == 1:
+        if idx % 2 == 0:
             player['Ben'] += 1
         else:
             player['Maria'] += 1
 
+    print(player)
     if player['Ben'] == player['Maria']:
         return None
     elif player['Ben'] > player['Maria']:
