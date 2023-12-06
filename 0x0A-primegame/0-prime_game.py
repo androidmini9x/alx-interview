3fddbcad28921f22ff5e115dd9d92a0b0a9d85e4
atck3r <atck3r@localdomain>
2023-12-06 19:11:46 +0300

    Test

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,62 +1,41 @@
 #!/usr/bin/python3
-"""
-This module contains the function `isWinner`
-which determines the winner of the game.
-"""
+
+""" Task 0. Prime Game """
 
 
-def is_prime(num):
-    """
-    Checks if a number is prime.
-    Returns True if the number is prime, False otherwise.
-    """
-    if num < 2:
+def is_prime(number):
+    """Checks if a number is prime or not"""
+    if number < 2:
         return False
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
+    for i in range(2, int(number**1/2) + 1):
+        if number % i == 0:
             return False
     return True
 
 
+def maria_vs_ben(n):
+    """Determines if Maria or Ben wins after a turn"""
+    prime_count = 0
+    for i in range(2, n + 1):
+        if is_prime(i):
+            prime_count += 1
+    return prime_count % 2 == 1
+
+
 def isWinner(x, nums):
-    """
-    Determines the winner of the game based on
-    the number of rounds and the values of `nums`.
+    """Returns the name of the player that won the most rounds"""
 
-    Args:
-        x (int): The number of rounds.
-        nums (list): A list of integers representing the
-        values of `n` for each round.
+    if x < 1 or x >= 10000 or not nums:
+        return None
 
-    Returns:
-        str or None: The name of the player with the
-        most wins (either "Maria" or "Ben"),
-        or None if the winner cannot be determined.
-    """
     maria_wins = 0
     ben_wins = 0
 
-    for i in range(x):
-        primes = []
-        for j in range(2, nums[i] + 1):
-            if is_prime(j):
-                primes.append(j)
-
-        m_turn = len(primes) % 2 == 0
-        b_turn = not m_turn
-
-        while primes:
-            if m_turn:
-                primes.pop()
-            else:
-                primes.pop(0)
-            m_turn = not m_turn
-            b_turn = not b_turn
-
-        if m_turn:
-            ben_wins += 1
-        else:
+    for n in nums:
+        if maria_vs_ben(n):
             maria_wins += 1
+        else:
+            ben_wins += 1
 
     if maria_wins > ben_wins:
         return "Maria"
